ed2bd41274b744b60615f88a977e386e4e805704
CITS <cits@CITSs-MBP-4.lan>
2024-11-13 17:59:09 -0500

    first commit

---
+++ src/components/Product.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Product = () => {
+    <div>
+     <img src="https://bit.ly/34clVdw" alt=""/>
+     <h1>I'm a Product!</h1>
+     <p>$19.99</p>
+     <p>
+        This is a very cool Product and buying it will make you 
+        fancier
+     </p>
+     </div>
+}
+
+export default Product
